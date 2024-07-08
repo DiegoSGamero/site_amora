27fd38650ba09d42fa7770ca65a2af10d1207e27
diego joao <diegosgjoao@gmail.com>
2024-07-08 16:21:19 -0300

    setup bootstrap

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+
+  def home; end
+
+  def historia; end
+
+  def municipios; end
+
+  def patrimonio_cultural; end
+
+  def patrimonio_natural; end
+
+  def legislacao; end
+end
