1923d4c89ec5cc91c7fb457ae066b173ccf3ebbb
diego joao <diegosgjoao@gmail.com>
2024-07-08 17:20:32 -0300

    starting with content and style

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get "/historia", to: "pages#historia"
+  get "/cidades", to: "pages#cidades"
+  get "/patrimonio_cultural", to: "pages#patrimonio_cultural"
+  get "/patrimonio_natural", to: "pages#patrimonio_natural"
+  get "/legislacao", to: "pages#legislacao"
 end
